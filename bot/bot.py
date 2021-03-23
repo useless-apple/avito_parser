ec169df4bd7160712c39e52fc9110a3be5e30955
useless-apple <danil16m@mail.ru>
2021-03-23 21:58:01 +0500

    First start

--- bot/bot.py
+++ bot/bot.py
@@ -1,16 +1,11 @@
 import telebot
 
-#main variables
+#Токен Вашего бота телеграм
 TOKEN = "1763434220:AAHwjEuTltLljp6rTfGf0gLSBe-LUE7J5vU"
 bot = telebot.TeleBot(TOKEN)
-#handlers
-@bot.message_handler(commands=['start', 'go'])
-def start_handler(message):
-    bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду парсить заголовки с хабра')
 
 @bot.message_handler(content_types=['text'])
 def text_handler(text):
-    #text = message.text.lower()
     chat_id = '-1001487777112'
     bot.send_message(chat_id, text)
 
--- main.py
+++ main.py
@@ -8,9 +8,10 @@ import cfscrape
 
 from bot.bot import text_handler
 
+#Указываем ссылку на запрос
 main_url = 'https://www.avito.ru/orenburg/doma_dachi_kottedzhi/prodam-ASgBAgICAUSUA9AQ?f=ASgBAQECAUSUA9AQAUDYCCTKWc5ZAUXAExh7ImZyb20iOm51bGwsInRvIjoxNDY1Mn0'
-#headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
+#Если нужно сгенерировать файл CSV
 def write_csv(result):
     with open('file.csv', 'a', encoding='utf-8') as f:
         writer = csv.writer(f)
@@ -23,6 +24,7 @@ def write_csv(result):
                               item['url']
                               ))
 
+#если делать по cron то путь к .db файлу должен быть полностью, иначе cron остановит скрипт.
 def write_sqlite3(result):
     conn = sqlite3.connect("avito_list.db")
     with conn:
@@ -36,19 +38,17 @@ def write_sqlite3(result):
 
                 cur.execute('SELECT avito_id FROM offers WHERE avito_id=?', (sql_avito_id,))
                 item_id = cur.fetchall()
-                print(item_id)
-                print(sql_avito_id)
 
                 if (item_id == [(sql_avito_id,)]):
-                    print('yes')
-
+                    print('ID found')
                     cur.execute('SELECT price FROM offers WHERE avito_id=?', (sql_avito_id,))
                     item_price = cur.fetchall()
+
                     if(item_price == [(sql_price,)]):
                         print('Price ok')
                         continue
                     else:
-                        text_handler(str(sql_avito_id) + ' id - обновилась цена \n Старая цена = ' + str(item_price[0][0]) + ' руб. / Новая цена = ' + str(sql_price) + ' руб.\n\nСсылка ' + str(sql_url))
+                        text_handler('Обновилась цена id ' + str(sql_avito_id) + '\n Старая цена = ' + str(item_price[0][0]) + ' руб. / Новая цена = ' + str(sql_price) + ' руб.\n\nСсылка ' + str(sql_url))
                         cur.execute("UPDATE offers SET price=? WHERE avito_id=?", (sql_price, sql_avito_id))
                         print('Price update')
                         time.sleep(5)
@@ -99,8 +99,8 @@ def get_page_data(page_url):
 
 def main(main_url):
     session = get_session()
+    #указываем доп фильтрацию по цене и для первого запроса страницу пагинации 1
     r=session.get(main_url + '&pmax=3500000&pmin=2500000&p=1')
-    print(main_url + '?p=1&pmax=3500000&pmin=2500000')
     if r.status_code == 200:
         soup=BeautifulSoup(r.text, 'html.parser')
         count_page = soup.find_all('span',{"class":"pagination-item-1WyVp"})[-2].text
@@ -109,8 +109,8 @@ def main(main_url):
             value = random.random()
             scaled_value = 1 + (value * (9 - 5))
             print('Parsing page# ' + str(i) + ' of ' + count_page)
+            #Для последующих страниц, пагинацию генерим
             page_url = main_url + '&pmax=3500000&pmin=2500000&p=' + str(i)
-            print(page_url)
             result+=get_page_data(page_url)
             time.sleep(scaled_value)
 

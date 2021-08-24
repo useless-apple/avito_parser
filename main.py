c17fa428db8db039b4d9211a7c990f73a0c7fa99
useless-apple <danil16m@mail.ru>
2021-08-24 15:14:58 +0500

    Work with telegram bot Add percent and difference price

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 import time
 import random
 import sqlite3
 import cfscrape
 from datetime import datetime
 import json
+
 from bot.bot import text_handler
 
 emoji_top = u'\U0001F4C8'
@@ -14,11 +16,9 @@ emoji_top_green = u'\U00002705'
 emoji_down = u'\U0001F4C9'
 emoji_down_red = u'\U0000274C'
 
-route_db = "avito_database.db"
-
 
 def write_sqlite3(result):
-    conn = sqlite3.connect(route_db)
+    conn = sqlite3.connect("avito_database.db")
     for url in result:
         with conn:
             cur = conn.cursor()
@@ -29,6 +29,7 @@ def write_sqlite3(result):
                 sql_price = url[0][i]['price']
                 sql_address = url[0][i]['address']
                 sql_url = url[0][i]['url']
+
                 price_history = []
                 price_now = [str(datetime.utcnow()), str(sql_price)]
 
@@ -43,7 +44,36 @@ def write_sqlite3(result):
                     price_history = json.loads(cur.fetchall()[0][0])
                     price_history += price_now
                     price_history_dumps = json.dumps(price_history)
-                    if (item_price == [(sql_price,)]):
+                    price_history_srt = ''
+
+                    if len(price_history) > 0:
+                        for i in range(0, len(price_history), 2):
+                            if i != 0 and i != 1:
+                                if price_history[i + 1] > price_history[i - 1]:
+                                    percent_price_history = '+ ' + str(round(
+                                        ((int(price_history[i + 1]) - int(price_history[i - 1])) / int(
+                                            price_history[i - 1])) * 100,
+                                        2))
+                                else:
+                                    percent_price_history = '- ' + str(round(((int(price_history[i - 1]) - int(
+                                        price_history[i + 1])) / int(price_history[i + 1])) * 100, 2))
+
+                                price_history_srt = price_history_srt + 'Дата: ' + price_history[i][0:10] + '  Цена:' + \
+                                                    price_history[i + 1] + ' руб.  (' + percent_price_history + '%)\n'
+                            else:
+                                price_history_srt = price_history_srt + 'Дата: ' + price_history[i][0:10] + '  Цена:' + \
+                                                    price_history[i + 1] + ' руб.\n'
+
+                        if price_history[1] > price_now[1]:
+                            difference_price = '- ' + str(int(price_history[1]) - int(price_now[1]))
+                            percent_difference_price = '- ' + str(
+                                round(((int(price_history[1]) - int(price_now[1])) / int(price_now[1])) * 100, 2))
+                        else:
+                            difference_price = '+ ' + str(int(price_now[1]) - int(price_history[1]))
+                            percent_difference_price = '+ ' + str(
+                                round(((int(price_now[1]) - int(price_history[1])) / int(price_history[1])) * 100, 2))
+
+                    if item_price == [(sql_price,)]:
                         # print('Price ok')
                         cur.execute("UPDATE offers SET status=1, updated_date=? WHERE avito_id=? AND city =?",
                                     (str(datetime.utcnow()), sql_avito_id, url[1][0]))
@@ -52,12 +82,16 @@ def write_sqlite3(result):
                         if (item_price >= [(sql_price,)]):
                             text_handler(url[1][1], 'Обновилась цена id ' + str(
                                 sql_avito_id) + '  ' + emoji_down + emoji_down + emoji_top_green + '\n Старая цена = ' + str(
-                                item_price[0][0]) + ' руб. / Новая цена = ' + str(sql_price) + ' руб.\n\nАдрес: ' + str(
+                                item_price[0][0]) + ' руб. / Новая цена = ' + str(
+                                sql_price) + ' руб.\n\nИзменения цен \n' + str(
+                                price_history_srt) + '\nРазница: ' + difference_price + ' (' + percent_difference_price + '%)\nАдрес: ' + str(
                                 sql_address) + '\n\nСсылка ' + str(sql_url))
                         else:
                             text_handler(url[1][1], 'Обновилась цена id ' + str(
                                 sql_avito_id) + '  ' + emoji_top + emoji_top + emoji_down_red + '\n Старая цена = ' + str(
-                                item_price[0][0]) + ' руб. / Новая цена = ' + str(sql_price) + ' руб.\n\nАдрес: ' + str(
+                                item_price[0][0]) + ' руб. / Новая цена = ' + str(
+                                sql_price) + ' руб.\n\nИзменения цен \n' + str(
+                                price_history_srt) + '\nРазница: ' + difference_price + ' (' + percent_difference_price + '%)\n\nАдрес: ' + str(
                                 sql_address) + '\n\nСсылка ' + str(sql_url))
 
                         cur.execute(
@@ -77,8 +111,7 @@ def write_sqlite3(result):
                     price_history += price_now
                     price_history_dumps = json.dumps(price_history)
                     cur.execute(
-                        "INSERT OR IGNORE INTO offers ('avito_id','name','price','price_history','address','url',"
-                        "'created_date','updated_date','status','city') VALUES (?,?,?,?,?,?,?,?,?,?)",
+                        "INSERT OR IGNORE INTO offers ('avito_id','name','price','price_history','address','url','created_date','updated_date','status','city') VALUES (?,?,?,?,?,?,?,?,?,?)",
                         (sql_avito_id, sql_name, sql_price, str(price_history_dumps), sql_address, sql_url,
                          str(datetime.utcnow()), str(datetime.utcnow()), 1, url[1][0]))
                     time.sleep(5)
@@ -109,6 +142,7 @@ def get_page_data(page_url):
     session = get_session()
     r = session.get(page_url)
     if r.status_code == 200:
+        time.sleep(1)
         soup = BeautifulSoup(r.text, 'html.parser')
         table = soup.find('div', {"data-marker": "catalog-serp"})
         if table:
@@ -125,23 +159,28 @@ def get_page_data(page_url):
                 try:
                     avito_id = int(row.get('data-item-id'))
                 except:
-                    avito_id = 0
+                    avito_id = 'Не найден'
+
                 try:
                     name = clean(row.find('h3', {"itemprop": "name"}).text)
                 except:
-                    name = ''
+                    name = 'Не найден'
+
                 try:
                     price = int(clean(row.find('meta', {"itemprop": "price"}).get("content")))
                 except:
-                    price = 0
+                    price = 'Не найден'
+
                 try:
                     url = 'https://avito.ru' + row.find('a', {"itemprop": "url"}).get("href")
                 except:
-                    url = ''
+                    url = 'Не найден'
+
                 try:
-                    address = clean(row.find('span', {"class": "geo-address-9QndR"}).text)
+                    address = clean(row.find('div', {"data-marker": "item-address"}).div.span.span.text)
                 except:
-                    address = ''
+                    address = 'Не найден'
+
                 item = {'avito_id': avito_id, 'name': name, 'price': price, 'address': address, 'url': url, }
                 result.append(item)
     else:
@@ -151,7 +190,7 @@ def get_page_data(page_url):
 
 
 def get_urls():
-    conn = sqlite3.connect(route_db)
+    conn = sqlite3.connect("avito_database.db")
     with conn:
         cur = conn.cursor()
         cur.execute('SELECT name,city,chatid FROM urls')
@@ -170,12 +209,20 @@ def main(main_url):
     for task in main_url:
         url_task = task[0]
         task = [task[1], task[2]]
+
         print(url_task)
         session = get_session()
         r = session.get(url_task + '&p=1')
         if r.status_code == 200:
             soup = BeautifulSoup(r.text, 'html.parser')
-            count_page = soup.find_all('span', {"class": "pagination-item-1WyVp"})[-2].text
+            try:
+                pagination = soup.find('div', {"data-marker": "pagination-button"})
+                pagination.find('span', {"data-marker": "pagination-button/prev"}).decompose()
+                pagination.find('span', {"data-marker": "pagination-button/next"}).decompose()
+                count_page = pagination.find_all('span')[-1].text
+            except:
+                count_page = 1
+                print('Error pagination')
             result = []
             for i in range(1, int(count_page) + 1):
                 value = random.random()
@@ -197,6 +244,10 @@ def main(main_url):
 
 
 if __name__ == '__main__':
-    main_url = []
-    main_url += get_urls()
-    main(main_url)
+    # main_url = []
+    # main_url += get_urls()
+    # main(main_url)
+    # print(main_url)
+    with open('data.json', encoding='utf-8', newline='') as json_file:
+        data = json.load(json_file)
+        write_sqlite3(data)

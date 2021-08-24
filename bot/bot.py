d4ac9a2b2d32b9cb64a20b388a2ca0c25f067f79
useless-apple <danil16m@mail.ru>
2021-08-24 19:14:34 +0500

    Work with telegram bot

--- bot/bot.py
+++ bot/bot.py
@@ -2,9 +2,7 @@ import telebot
 
 from settings import TG_TOKEN
 
-#Токен Вашего бота телеграм
-TOKEN = TG_TOKEN
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(TG_TOKEN)
 
 @bot.message_handler(content_types=['text'])
 def text_handler(chat_id, text):

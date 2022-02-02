055fa19306d1b2062d0f9576156ab30850c45072
useless-apple <danil16m@mail.ru>
2022-02-02 17:58:28 +0500

    finish rework price_history

--- sqlite.py
+++ sqlite.py
@@ -27,7 +27,10 @@ def write_sqlite3(url):
                 sql_params = url[0][i]['params']
 
                 price_history = []
-                price_now = [str(get_date_time()), str(sql_price)]
+                price_now = {
+                    "data": str(get_date_time()),
+                    "price": str(sql_price)
+                }
 
                 cur.execute('SELECT avito_id FROM offers WHERE avito_id=?',
                             (sql_avito_id,))
@@ -44,26 +47,26 @@ def write_sqlite3(url):
                                 (sql_avito_id,))
 
                     price_history = json.loads(cur.fetchall()[0][0])
-                    price_history += price_now
+                    price_history.append(price_now)
                     price_history_dumps = json.dumps(price_history)
 
                     price_history_srt = ''
                     if len(price_history) > 0:
-                        for i in range(0, len(price_history), 2):
-                            if i != 0 and i != 1:
-                                percent_price_history = calculation_percent(price_history[i - 1], price_history[i + 1])
+                        for i in range(0, len(price_history)):
+                            if i == 0:
                                 price_history_srt = price_history_srt + \
-                                                    'Дата: ' + price_history[i][0:10] + '  ' + \
-                                                    'Цена: ' + num_conversion(price_history[i + 1]) + ' руб.  ' + \
+                                                    'Дата: ' + price_history[i]['data'] + '  ' + \
+                                                    'Цена: ' + num_conversion(price_history[i]['price']) + ' руб.\n'
+                            else:
+                                percent_price_history = calculation_percent(price_history[i - 1]['price'],
+                                                                            price_history[i]['price'])
+                                price_history_srt = price_history_srt + \
+                                                    'Дата: ' + price_history[i]['data'] + '  ' + \
+                                                    'Цена: ' + num_conversion(price_history[i]['price']) + ' руб.  ' + \
                                                     '(' + percent_price_history + '%)\n'
 
-                            else:
-                                price_history_srt = price_history_srt + \
-                                                    'Дата: ' + price_history[i][0:10] + '  ' + \
-                                                    'Цена: ' + num_conversion(price_history[i + 1]) + ' руб.\n'
-
-                        difference_price = calculation_different_price(price_history[1], price_now[1])
-                        percent_difference_price = calculation_percent(price_history[1], price_now[1])
+                        difference_price = calculation_different_price(price_history[0]['price'], price_now['price'])
+                        percent_difference_price = calculation_percent(price_history[0]['price'], price_now['price'])
 
                     if item_price == [(sql_price,)]:  # Сравниваем цены, и если есть отличие то обновляем их
                         cur.execute(
@@ -91,7 +94,7 @@ def write_sqlite3(url):
                                     sql_type_of, 'new')
                     log.info('No ID -> New Offer | ' + str(sql_avito_id))
 
-                    price_history += price_now
+                    price_history.append(price_now)
                     price_history_dumps = json.dumps(price_history)
                     cur.execute(
                         "INSERT OR IGNORE INTO offers ('avito_id','name','price','price_history','address','url','created_date','updated_date','status','city','urls_id','type_of','params') VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",

5807cd9fbf17361d9cec0a6b173e471704f38dbc
Даниил <69340849+useless-apple@users.noreply.github.com>
2022-02-02 12:32:50 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 #Относительный путь до проекта (нужно для крона)
-DIR_LOCATION = ""
+DIR_LOCATION = "/root/parser/"
 
 #Путь до базы данных
 ROUTE_DB = DIR_LOCATION + "avito_database.db"
 
 #Телеграм токен бота
-TG_TOKEN = '1763434220:AAGeNeRuUOtUyLwZRGfuT2t_Q01Xv3ind_E'
+TG_TOKEN = 'NNNNNNNNNNNNNNNNNNN'
 
 #ID чата для отлавливания ошибок
-EXEPTION_CHAT = '-521939372'
+EXEPTION_CHAT = 'NNNNNNNNNNNNNN'
